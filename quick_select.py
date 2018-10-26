53f944cb68fa030573b3f5069d3485752340786b
孙冠雄 <747166863@qq.com>
2018-10-27 01:36:47 +0800

    Update quick_select.py

--- quick_select.py
+++ quick_select.py
@@ -2,41 +2,36 @@ def kth_biggest(k, nums, begin, end):
     if k<0 or len(nums)<1 or begin>end or k>end-begin+1:
         return -1
     else:
-        if k == end - begin + 1:
-            temp = nums[begin:end + 1]
-            temp.sort()
-            return temp[0];
         # k < end - begin + 1
-        else:
-            pivot = nums[begin];
-            head = begin;
-            rear = end;
-            while head < rear:
-                while nums[head] <= pivot and head < rear:
-                    head += 1
-                while nums[rear] > pivot and head < rear:
-                    rear -= 1
-                swap(nums, head, rear)
-            # judge nums[head=rear] and pivot
-            if nums[head] > pivot:
-                num_bigger = end - head + 1
-                if num_bigger == k-1:
-                    return pivot
-                elif num_bigger > k-1:
-                    return kth_biggest(k, nums, head, end)
-                else:
-                    return kth_biggest(k-num_bigger, nums, begin, head-1)
+        pivot = nums[begin];
+        head = begin;
+        rear = end;
+        while head < rear:
+            while nums[head] <= pivot and head < rear:
+                head += 1
+            while nums[rear] > pivot and head < rear:
+                rear -= 1
+            swap(nums, head, rear)
+        # judge nums[head=rear] and pivot
+        if nums[head] > pivot:
+            num_bigger = end - head + 1
+            if num_bigger == k-1:
+                return pivot
+            elif num_bigger > k-1:
+                return kth_biggest(k, nums, head, end)
             else:
-                swap(nums, 0, head)
-                num_bigger = end - head
-                if k == 1:
-                    return pivot
-                if num_bigger == k-1:
-                    return pivot
-                elif num_bigger > k-1:
-                    return kth_biggest(k, nums, head+1, end)
-                else:
-                    return kth_biggest(k - num_bigger, nums, begin, head-1)
+                return kth_biggest(k-num_bigger, nums, begin, head-1)
+        else:
+            swap(nums, begin, head)
+            num_bigger = end - head
+            if k == 1:
+                return pivot
+            if num_bigger == k-1:
+                return pivot
+            elif num_bigger > k-1:
+                return kth_biggest(k, nums, head+1, end)
+            else:
+                return kth_biggest(k - num_bigger, nums, begin, head-1)
     return
 
 
