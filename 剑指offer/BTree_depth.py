fdba20ca902439f499ec8fc9f61d08a4093d15f6
孙冠雄 <747166863@qq.com>
2018-11-15 15:15:06 +0800

    层序遍历

--- 剑指offer/BTree_depth.py
+++ 剑指offer/BTree_depth.py
@@ -6,10 +6,10 @@
 #         self.right = None
 """
 1.递归，depth = max(depth_left, depth_right) + 1
-2.非递归
+2.层序遍历，queue
+3.前序、中序、后序
 
 """
-
 # class Solution:
 #     def TreeDepth(self, pRoot):
 #         # write code here
@@ -19,41 +19,28 @@
 #         depth_right = self.TreeDepth(pRoot.right)
 #         return max(depth_left, depth_right) + 1
 
+# class Solution:
+#     def TreeDepth(self, pRoot):
+#         # write code here
+#         if pRoot is None:
+#             return 0
+#         queue = [pRoot]
+#         max_depth = 0
+#         while queue:
+#             for i in range(len(queue)):
+#                 cur = queue.pop(0)
+#                 if cur.left:
+#                     queue.append(cur.left)
+#                 if cur.right:
+#                     queue.append(cur.right)
+#             max_depth += 1
+#         return max_depth
+
 class Solution:
     def TreeDepth(self, pRoot):
         # write code here
-        cur = pRoot
         if pRoot is None:
             return 0
-        # stack = [pRoot]
-        # times = [1]
-        max_depth = 1
-        cur_depth = 1
-        # while stack:
-        #     print stack
-        #     while cur.left and times[-1] is not 3:
-        #         cur = cur.left
-        #         stack.append(cur)
-        #         times.append(1)
-        #         cur.flag = True
-        #         cur_depth += 1
-        #         if cur_depth > max_depth:
-        #             max_depth = cur_depth
-        #     times[-1] += 1
-        #     if cur.right and times[-1] is not 3:
-        #         cur = cur.right
-        #         cur_depth += 1
-        #         if cur_depth > max_depth:
-        #             max_depth = cur_depth
-        #         cur = cur.right
-        #         stack.append(cur)
-        #         times.append(1)
-        #     else:
-        #         stack.pop()
-        #         times.pop()
-        #         times[-1] += 1
-        #         cur_depth -= 1
-        #         cur = stack[-1]
         queue = [pRoot]
         max_depth = 0
         while queue:
