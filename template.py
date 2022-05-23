9fed61d43c23e5a40672beff4e7e91685b768115
Duckduckgod <gsun02@qub.ac.uk>
2022-05-23 22:20:11 +0100

    update

---
+++ template.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+import math
+
+
+class Solution:
+    def __init__(self) -> None:
+        pass
+
+    def function(self, arg1, arg2):
+        pass
+
+def main():
+    s = Solution()
+    print (s.function(1, 2))
+
+if __name__ == '__main__':
+    main()
