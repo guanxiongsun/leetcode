5459353d72217af93a4eb88201ac996a280e41e8
Duckduckgod <gsun02@qub.ac.uk>
2022-05-23 22:10:20 +0100

    update

--- JZOffer/find_in_2d_array.py
+++ JZOffer/find_in_2d_array.py
@@ -39,5 +39,5 @@ class Solution:
                 return True
             return False
 
-# s = Solution()
-# print (s.Find(7,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]))
+s = Solution()
+print (s.Find(7,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]))
